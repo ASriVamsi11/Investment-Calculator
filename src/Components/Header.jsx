80da18515658b1e0199a1b3960d2eb910ce8cde1
ASriVamsi11 <102286427+ASriVamsi11@users.noreply.github.com>
2025-06-19 19:42:01 -0600

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@
 export default function Header() {
     return (
         <header id="header">
-            <img src="investment-calculator-logo.png" alt=""/>
+            <img src="/investment-calculator-logo.png" alt="logo"/>
             <h1>Investment Calculator</h1>
         </header>
     )
